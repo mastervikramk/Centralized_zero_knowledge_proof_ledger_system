1c79748e9a2c1e7a30de192c65f26cfbf886c6af
Vikram Kumar <106912119+Mrnoobcoder@users.noreply.github.com>
2024-02-14 19:57:03 +0530

    modifications with staticmethod

--- Database/keys.py
+++ Database/keys.py
@@ -1,34 +1,34 @@
-
 import ecdsa
 from mnemonic import Mnemonic
 import hashlib
 import base58
 
 class KeyPair:
-    #Generating private and public keys
-    @staticmethod
-    def generate_crypto_key_pair():
-        private_key = ecdsa.SigningKey.generate()
-        public_key = private_key.get_verifying_key()
-        return private_key, public_key
+    def __init__(self):
+        self.private_key = None
+        self.public_key = None
     
-    #Generating BIP-39 mnemonic of 18 words from private key (hex)
-    @staticmethod
-    def private_key_to_mnemonic(private_key):
-        private_key_bytes = bytes.fromhex(private_key)
+    # Generating private and public keys
+    def create(self):
+        self.private_key = ecdsa.SigningKey.generate()
+        self.public_key = self.private_key.get_verifying_key()
+    
+    # Generating BIP-39 mnemonic of 18 words from private key (hex)
+    def private_key_to_mnemonic(self):
+        private_key_bytes = self.private_key.to_string()
         return Mnemonic("english").to_mnemonic(private_key_bytes)
  
-    #Converting mnemonic back to private key (hex)
+    # Generating address
+    def create_address(self):
+        public_key_bytes = self.public_key.to_string()
+        sha256_hash = hashlib.sha256(public_key_bytes).digest()
+        ripemd160_hash = hashlib.new('ripemd160', sha256_hash).digest()
+        address = base58.b58encode(ripemd160_hash)
+        return address.decode('utf-8')
+    
+    # Generating BIP-39 mnemonic from private key
     @staticmethod
     def mnemonic_to_private_key(mnemonic):
         entropy = Mnemonic("english").to_entropy(mnemonic)
         return entropy.hex()
-    
-    #Generating address
-    @staticmethod
-    def generate_crypto_address(public_key):
-        public_key_bytes = public_key.to_string()
-        sha256_hash = hashlib.sha256(public_key_bytes).digest()#sha256 hashing
-        ripemd160_hash = hashlib.new('ripemd160', sha256_hash).digest()#ripemd160 hashing
-        address = base58.b58encode(ripemd160_hash)#base58 encoding
-        return address.decode('utf-8')
+
