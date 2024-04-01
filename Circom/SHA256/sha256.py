ca34b3e9d2fd432c6118f20c20062e40a6361078
Mrnoobcoder <vikrammuz43@gmail.com>
2024-04-01 16:42:38 +0530

    class for hash and bitarray

--- Circom/SHA256/sha256.py
+++ Circom/SHA256/sha256.py
@@ -2,31 +2,47 @@ import hashlib
 import json
 
 
-def bits(bytestr):
-    return ''.join(format(byte, '08b') for byte in bytestr)
+class HashGenerator:
+    def __init__(self):
+        pass
 
+    @staticmethod
+    def bits(bytestr):
+        return ''.join(format(byte, '08b') for byte in bytestr)
 
-def bitarray(bytestr):
-    return [int(b) for b in bits(bytestr)]
+    @staticmethod
+    def bitarray(bytestr):
+        return [int(b) for b in HashGenerator.bits(bytestr)]
 
+    @staticmethod
+    def sha256_hash(utxo_id, output_address, num):
+        concatenated_input = HashGenerator.int_to_byte(utxo_id) + output_address.encode() + HashGenerator.int_to_byte(num)
+        sha256 = hashlib.sha256(concatenated_input)
+        print(f"input={HashGenerator.bits(concatenated_input)}")
+        print(f'sha hex={sha256.hexdigest()}')
+        print(f'sha bin={HashGenerator.bits(sha256.digest())}')
+        print(f'sha bin reverse={HashGenerator.bits(sha256.digest())[::-1]}')
+        return sha256.digest()
 
-def sha256_hash(inputs):
-    concatenated_input = b''.join(inputs)
-    sha256 = hashlib.sha256(concatenated_input)
-    print(f"input={bits(concatenated_input)}")
-    print(f'sha hex={sha256.hexdigest()}')
-    print(f'sha bin={bits(sha256.digest())}')
-    print(f'sha bin reverse={bits(sha256.digest())[::-1]}')
-    return sha256.digest()
+    @staticmethod
+    def int_to_byte(num):
+        assert 0 <= num < 256
+        return num.to_bytes(1, byteorder='big', signed=False)
 
-
-def int_to_byte(num):
-    assert 0 <= num < 256
-    return num.to_bytes(1, byteorder='big', signed=False)
+    def generate_hash(self, utxo_id, output_address, amount):
+        output = HashGenerator.sha256_hash(utxo_id, output_address, amount)
+        io = {
+            "utxo_id": HashGenerator.bitarray(HashGenerator.int_to_byte(utxo_id)),
+            "output_address": HashGenerator.bitarray(output_address.encode()),
+            "amount": HashGenerator.bitarray(HashGenerator.int_to_byte(amount)),
+            "hash": HashGenerator.bitarray(output)
+        }
+        return json.dumps(io)
 
 
 if __name__ == "__main__":
-    inputs = [int_to_byte(1), b'2Y1aAMVLzhoeBvEm6antX1EbP5PR',int_to_byte(30)]  # Example inputs
-    output = sha256_hash(inputs)
-    io = {"inputs": [bitarray(inp) for inp in inputs], "hash": bitarray(output)}
-    print(json.dumps(io))
+    hash_generator = HashGenerator()
+    utxo_id = 1
+    output_address = '2Y1aAMVLzhoeBvEm6antX1EbP5PR'
+    amount = 30
+    print(hash_generator.generate_hash(utxo_id, output_address, amount))
