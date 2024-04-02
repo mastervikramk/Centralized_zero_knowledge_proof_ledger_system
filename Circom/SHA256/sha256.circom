672bca7cdbed857543f0c0411deb2dc14cf2c205
Mrnoobcoder <vikrammuz43@gmail.com>
2024-04-02 16:43:06 +0530

    new circuit with 8 bytes no

--- Circom/SHA256/sha256.circom
+++ Circom/SHA256/sha256.circom
@@ -5,38 +5,47 @@ include "./circomlib/circuits/bitify.circom";
 
 template SHA256Hasher() {
     // Define input signals
-    signal input utxo_id[8];
+    signal input utxo_id[64];
     signal input output_address[224];
-    signal input amount[8];
+    signal input amount[64];
+    signal input signature[768];
+
 
     // Define hash signals
     signal input hash[256];
     signal output chash[256];
+
+    var utxo=64,addr=224,amt=64,sig=768;
     
+    var total=utxo+addr+amt+sig;
     // Concatenate all input arrays into a single input array
-    signal input_concat[240]; // Total length of all input arrays
+    signal input_concat[total]; // Total length of all input arrays
 
     // Copy utxo_id to input_concat
-    for (var i = 0; i < 8; i++) {
+    for (var i = 0; i < utxo; i++) {
         input_concat[i] <== utxo_id[i];
     }
 
-
     // Copy output_address to input_concat
-    for (var i = 0; i < 224; i++) {
-        input_concat[8 + i] <== output_address[i];
+    for (var i = 0; i < addr; i++) {
+        input_concat[utxo + i] <== output_address[i];
     }
 
     // Copy amount to input_concat
-    for (var i = 0; i < 8; i++) {
-        input_concat[232 + i] <== amount[i];
+    for (var i = 0; i < amt; i++) {
+        input_concat[utxo+addr + i] <== amount[i];
+    }
+
+    // Copy amount to input_concat
+    for (var i = 0; i < sig; i++) {
+        input_concat[utxo+addr+amt + i] <== signature[i];
     }
 
     // Instantiate the SHA-256 component with the concatenated input array
-    component sha256hasher = Sha256(240);
+    component sha256hasher = Sha256(total);
 
     // Connect input_concat to sha256hasher
-    for (var i = 0; i < 240; i++) {
+    for (var i = 0; i < total; i++) {
         sha256hasher.in[i] <== input_concat[i];
     }
 
--- Circom/SHA256/sha256.py
+++ Circom/SHA256/sha256.py
@@ -15,8 +15,8 @@ class HashGenerator:
         return [int(b) for b in HashGenerator.bits(bytestr)]
 
     @staticmethod
-    def sha256_hash(utxo_id, output_address, num):
-        concatenated_input = HashGenerator.int_to_byte(utxo_id) + output_address.encode() + HashGenerator.int_to_byte(num)
+    def sha256_hash(utxo_id, output_address, amount, signature):
+        concatenated_input = HashGenerator.int_to_byte(utxo_id) + output_address.encode() + HashGenerator.int_to_byte(amount)+ signature.encode()
         sha256 = hashlib.sha256(concatenated_input)
         print(f"input={HashGenerator.bits(concatenated_input)}")
         print(f'sha hex={sha256.hexdigest()}')
@@ -25,16 +25,18 @@ class HashGenerator:
         return sha256.digest()
 
     @staticmethod
-    def int_to_byte(num):
-        assert 0 <= num < 256
-        return num.to_bytes(1, byteorder='big', signed=False)
+    def int_to_byte(num, num_bytes=8):
+        assert 0 <= num < 256 ** num_bytes
+        return num.to_bytes(num_bytes, byteorder='big', signed=False)
 
-    def generate_hash(self, utxo_id, output_address, amount):
-        output = HashGenerator.sha256_hash(utxo_id, output_address, amount)
+
+    def generate_hash(self, utxo_id, output_address, amount,signature):
+        output = HashGenerator.sha256_hash(utxo_id, output_address, amount,signature)
         io = {
             "utxo_id": HashGenerator.bitarray(HashGenerator.int_to_byte(utxo_id)),
             "output_address": HashGenerator.bitarray(output_address.encode()),
             "amount": HashGenerator.bitarray(HashGenerator.int_to_byte(amount)),
+            "signature":HashGenerator.bitarray(signature.encode()),
             "hash": HashGenerator.bitarray(output)
         }
         return json.dumps(io)
@@ -45,4 +47,5 @@ if __name__ == "__main__":
     utxo_id = 1
     output_address = '2Y1aAMVLzhoeBvEm6antX1EbP5PR'
     amount = 30
-    print(hash_generator.generate_hash(utxo_id, output_address, amount))
+    signature='5b270a90f3404d53d00541dee19ec8348f212e9fc2a15e448acaa435b05a6b4a8a4ca9131333f0932fdde0a4589e9d75'
+    print(hash_generator.generate_hash(utxo_id, output_address, amount,signature))
